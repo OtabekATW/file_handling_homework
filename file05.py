7f4f42a119f0aaa7a15d06107bd8d6693be300f3
OtabekATW <otabektursunov468@gmail.com>
2023-12-11 02:40:32 +0500

    file05.py

--- file05.py
+++ file05.py
@@ -6,5 +6,16 @@ def main(data:str):
     Returns:
         list: return answer
     """
+    f = open(data, 'r')
+    s = f.read()
+    
+    list1 = []
+    list2 = []
+    for i in s:
+        if i.isdigit():
+            list1.append(i)
+        else:
+            list2.append(i)
+    return [len(list1), len(list2)]
     
 # Read data from file
