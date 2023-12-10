71fc580cfb0bf162a33aa0ec114b633c1bdacc01
OtabekATW <otabektursunov468@gmail.com>
2023-12-11 03:39:31 +0500

    file10.py

--- file10.py
+++ file10.py
@@ -6,5 +6,15 @@ def main(data:str):
     Returns:
         int: return answer
     """
+    f = open(data, 'r')
+    s = f.read()
+
+    list1 = s.split('\n')
+    mx = len(list1[0])
+    for i in list1:
+        if mx < len(i):
+            mx = len(i)
+
+    return mx
 
 # Read data from file
