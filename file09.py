9b6128ef50c580a6a7df4ec9eba85cd438d24984
OtabekATW <otabektursunov468@gmail.com>
2023-12-11 21:45:02 +0500

    file09.py

--- file09.py
+++ file09.py
@@ -7,18 +7,11 @@ def main(data:str):
         int: return answer
     """
 
-    #list1 = data.split()
-
-    for i in data:
-        if i.isalpha():
-            data.remove(i)
-    
     list1 = []
     for i in data:
-        list1.append(i)
+        if i.isdigit():
+            list1.append(int(i))
     
     return min(list1)
 
 # Read data from file
-f = open('data/data09.txt')
-print(main(f.read()))
